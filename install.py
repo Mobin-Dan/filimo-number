8d8e760c8cf337ecd98876b6a3fd169f0f40c304
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-03-27 01:27:17 +0430

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,7 @@
 import os
-os.system("unzip filimo.zip")
-os.system("cd filimo")
+os.system("apt update")
+os.system("pip install requests")
+os.system("pip install bs4")
+os.system("pip install colorama")
+os.system("pkg install python")
 os.system("python filimo.py")
